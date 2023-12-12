ef9f4cb1d24129a5d9df98240fb9a70d67a631f1
marcprux <mwp1@cornell.edu>
2023-12-11 20:52:36 -0500

    Add pointer typealiases

--- Sources/SkipFFI/SkipFFI.swift
+++ Sources/SkipFFI/SkipFFI.swift
@@ -8,7 +8,17 @@
 /// A  JNA `com.sun.jna.Pointer` is the equivalent of a Swift `OpaquePointer`
 public typealias OpaquePointer = com.sun.jna.Pointer
 public typealias UnsafeMutableRawPointer = com.sun.jna.ptr.PointerByReference
-public typealias UnsafeMutablePointer<T> = UnsafeMutableRawPointer
+
+public typealias UnsafeMutablePointer<Element> = UnsafeMutableRawPointer
+public typealias UnsafeMutableBufferPointer<Element> = UnsafeMutableRawPointer
+public typealias UnsafeBufferPointer<Element> = UnsafeMutableRawPointer
+public typealias UnsafePointer<Pointee> = UnsafeMutableRawPointer
+public typealias UnsafeMutableRawBufferPointer = UnsafeMutableRawPointer
+public typealias UnsafeRawBufferPointer = UnsafeMutableRawPointer
+public typealias UnsafeRawPointer = UnsafeMutableRawPointer
+
+public typealias SkipFFIStructure = com.sun.jna.Structure
+
 
 public func withUnsafeMutablePointer<T>(to pointerRef: InOut<OpaquePointer?>, block: (UnsafeMutableRawPointer) throws -> T) rethrows -> T {
     let pref = UnsafeMutableRawPointer()
@@ -23,6 +33,13 @@ public func withUnsafeMutablePointer<T>(to pointerRef: InOut<OpaquePointer?>, bl
 public func String(cString: OpaquePointer) -> String {
     cString.getString(0)
 }
+
+//public func withUnsafeMutableBytes<T, Result>(of value: inout T, _ body: (Any /* UnsafeMutableRawBufferPointer */) throws -> Result) rethrows -> Result { fatalError() }
+//public func withUnsafeBytes<T, Result>(of value: inout T, _ body: (Any /* UnsafeRawBufferPointer */) throws -> Result) rethrows -> Result { fatalError() }
+//public func withUnsafeBytes<T, Result>(of value: T, _ body: (Any /* UnsafeRawBufferPointer */) throws -> Result) rethrows -> Result { fatalError() }
+//public func withUnsafeTemporaryAllocation<R>(byteCount: Int, alignment: Int, _ body: (Any /* UnsafeMutableRawBufferPointer */) throws -> R) rethrows -> R { fatalError() }
+//public func withUnsafeTemporaryAllocation<R>(of type: Any /* T.Type */, capacity: Int, _ body: (Any /* UnsafeMutableBufferPointer<T> */) throws -> R) rethrows -> R { fatalError() }
+
 
 
 #endif
